a3c2d0d9b78babd36d9d4890f0cf6b818e43e391
helium <19122248282@163.com>
2024-09-02 19:58:45 +0800

    grpc-ssl

--- hello-client/main.go
+++ hello-client/main.go
@@ -7,12 +7,18 @@ import (
 
 	pb "example.com/learn-grpc/hello-client/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
 func main() {
-	// 连接server端，此处禁用安全传输，没有加密和验证
-	conn, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// 配置ssl，"*.heliu.site"在实际开发中从浏览器中取获取，证书路径使用绝对路径
+	creds, _ := credentials.NewClientTLSFromFile(
+		"/root/workspace/learn-grpc/key/test.pem",
+		"*.heliu.site",
+	)
+
+	// 连接server端，使用ssl加密通信
+	conn, err := grpc.NewClient("127.0.0.1:9090", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -22,7 +28,12 @@ func main() {
 	// 建立连接
 	client := pb.NewSayHelloClient(conn)
 	// 执行rpc调用(这个方法在服务器端来实现并返回结构)
-	resp, _ := client.SayHello(context.Background(), &pb.HelloRequest{RequestName: "gh"})
+	resp, err := client.SayHello(context.Background(), &pb.HelloRequest{RequestName: "gh"})
+
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
 
 	fmt.Println(resp.GetResponseMsg())
 }
--- hello-server/main.go
+++ hello-server/main.go
@@ -7,13 +7,25 @@ import (
 
 	pb "example.com/learn-grpc/hello-server/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 func main() {
+	// 绝对地址
+	creds, err1 := credentials.NewServerTLSFromFile(
+		"/root/workspace/learn-grpc/key/test.pem",
+		"/root/workspace/learn-grpc/key/test.key",
+	)
+
+	if err1 != nil {
+		fmt.Printf("证书错误：%v", err1)
+		return
+	}
+
 	// 开启端口
 	listen, _ := net.Listen("tcp", ":9090")
 	// 创建grpc服务
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
 	// 在grpc服务端中注册我们自己编写的服务
 	pb.RegisterSayHelloServer(grpcServer, &server{})
 
@@ -31,6 +43,6 @@ type server struct {
 
 func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
 	return &pb.HelloResponse{
-		ResponseMsg: "hello" + req.RequestName,
+		ResponseMsg: "hello," + req.RequestName,
 	}, nil
 }
